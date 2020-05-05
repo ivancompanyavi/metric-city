c7d8f4205750d1f3fef26c3ed5103424a04231c7
Ivan Company <ivancompanyavi@gmail.com>
2020-05-05 06:19:45 -0700

    Removed unused css

--- src/components/config-bar/element-form.js
+++ src/components/config-bar/element-form.js
@@ -6,23 +6,6 @@ const template = /*html*/ `
     div.shown {
       display: block;
     }
-    .fieldset {
-      display: flex;
-      border: 1px solid var(--color-silver);
-      border-radius: 5px;
-      color: var(--color-silver);
-      margin-bottom: 15px;
-      padding: 10px;
-    }
-    input {
-      border: 0;
-      flex: 1;
-      background-color: var(--color-grey);
-      color: var(--color-silver);
-    }
-    label {
-      flex: 0;
-    }
     h3 {
       padding: 0 20px 20px 20px;
       margin: 0;
